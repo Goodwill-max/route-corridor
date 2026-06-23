cbbdecff1dd3112900c1eb466ab4471ea35b83be
Goodwill-max <sachanpankhuri20@gmail.com>
2026-06-23 20:56:45 +0530

    Initial route corridor visualization

---
+++ src/App.tsx
@@ -0,0 +1,106 @@
+import { MapContainer, TileLayer, Polyline ,CircleMarker,Polygon, } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import * as turf from "@turf/turf";
+import * as h3 from "h3-js";
+
+const route: [number, number][] = [
+  [26.5123, 80.2329],
+  [26.5180, 80.2400],
+  [26.5240, 80.2480],
+];
+
+const line = turf.lineString(
+  route.map(([lat, lng]) => [lng, lat])
+);
+
+const corridor = turf.buffer(line, 0.35, {
+  units: "kilometers",
+});     
+
+const corridorCoords =
+  corridor.geometry.coordinates[0].map(
+    ([lng, lat]) => [lat, lng] as [number, number]
+  );
+
+  
+
+const pickups: [number, number][] = [
+  [26.514, 80.235],
+  [26.519, 80.242],
+  [26.521, 80.246],
+  [26.528, 80.252],
+];
+
+const eligiblePickups = pickups.map((pickup) => {
+  const point = turf.point([
+    pickup[1], // lng
+    pickup[0], // lat
+  ]);
+
+  return turf.booleanPointInPolygon(
+    point,
+    corridor
+  );
+});
+
+const pickupCells = pickups.map(
+  ([lat, lng]) =>
+    h3.latLngToCell(lat, lng, 9)
+);
+
+console.log(pickupCells);
+
+function App() {
+  return (
+    <MapContainer
+      center={[26.5123, 80.2329]}
+      zoom={13}
+      style={{
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <TileLayer
+        attribution="&copy; OpenStreetMap contributors"
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      <Polyline
+        positions={route}
+        pathOptions={{
+        color: "red",
+        weight: 5,
+      }}
+      />
+      {pickups.map((p, idx) => (
+        <CircleMarker
+          key={idx}
+          center={p}
+          radius={6}
+          pathOptions={{
+          color: eligiblePickups[idx]
+            ? "green"
+            : "gray",
+
+          fillColor: eligiblePickups[idx]
+            ? "green"
+            : "gray",
+
+          fillOpacity: 1,
+       }}
+       />
+      ))}
+
+      <Polygon
+        positions={corridorCoords}
+        pathOptions={{
+        color: "green",
+        fillColor: "green",
+        fillOpacity: 0.3,
+      }}
+    />
+    </MapContainer>
+  );
+}
+
+export default App;
